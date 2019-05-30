1b225e94eb631b9f0f2a06f3dd4008fedf74c19e
KNewhart <kbnewhart@mines.edu>
2019-05-30 06:43:49 -0700

    Initial NN Results compiler

--- r-sq.R
+++ r-sq.R
@@ -1,7 +1,8 @@
 r.sq <- function(raw,fit) {
-  y <- raw
-  y_bar <- mean(raw)
-  y_hat <- fit
-  r_sq <- sum((y_hat-y_bar)^2) / sum((y-y_bar)^2)
+  # y <- raw
+  # y_bar <- mean(raw)
+  # y_hat <- fit
+  # r_sq <- sum((y_hat-y_bar)^2) / sum((y-y_bar)^2)
+  r_sq <- cor(raw,fit)^2
   return(r_sq)
 }
